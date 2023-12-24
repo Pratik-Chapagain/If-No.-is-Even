04591f0a76d57abc72787ebd1045c46a0639ad22
Pratik Chapagain <chapagainpratik28@gmail.com>
2023-12-24 09:41:18 +0545

    push

---
+++ code.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    if (n%2==0)
+    {
+        printf("The number is even");
+    }
+    return 0;
+}
